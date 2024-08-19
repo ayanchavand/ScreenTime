184d4d93c67adff8d1f2e77b2f616c3b96eb607c
ayanchavand <ayan.chavand15@gmail.com>
2024-08-20 01:11:04 +0530

    Implemented timer

--- ScreenTimeTracker-Frontend/src/Components/timer.jsx
+++ ScreenTimeTracker-Frontend/src/Components/timer.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import Time from "../utils/time";
+
 export default function Timer(){
+    const [time, setTime] = useState(0)
     
+    useEffect(() => {
+        let intervalId = setInterval(() => setTime(prevTime => prevTime + 1), 1000);
+
+        return () => clearInterval(intervalId); // Clear interval on unmount
+    }, []);
+
+    const hours = String(Math.floor(time / 3600)).padStart(2, '0');
+    const minutes = String(Math.floor((time % 3600) / 60)).padStart(2, '0');
+    const seconds = String(time % 60).padStart(2, '0');
+
+
     return(
         <div className='flex flex-col items-center justify-center h-screen'>
             <h1 className="text-9xl ">
-            00:00:02
+            {hours}:{minutes}:{seconds}
             </h1>
             <h5 className="">Current Session</h5>
         </div>
--- ScreenTimeTracker-Frontend/src/utils/time.js
+++
@@ -1,9 +0,0 @@
-class Time{
-    constructor(hour, minute, second){
-        this.hour = hour
-        this.minute = minute
-        this.second = second
-    }
-}
-
-export default Time
