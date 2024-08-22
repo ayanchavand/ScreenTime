5f477b335c8f428398cdca6258b3a3656ba3b0b0
ayanchavand <ayan.chavand15@gmail.com>
2024-08-22 21:50:30 +0530

    feat: current session fetch

--- Frontend/src/Components/timer.jsx
+++ Frontend/src/Components/timer.jsx
@@ -1,62 +1,78 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import {auth, firestore, doc, setDoc} from '../utils/firebase'
+    import React from "react";
+    import { useState, useEffect } from "react";
+    import {auth, firestore, doc, setDoc, getDoc} from '../utils/firebase'
 
 
-//TODO: break this into smaller components cuz this is basically the home page
-export default function Timer(){
-    const [time, setTime] = useState(0)
-    const userDoc = doc(firestore, 'users/' + auth.currentUser.uid) 
+    //TODO: break this into smaller components cuz this is basically the home page
+    export default function Timer(){
+        const [time, setTime] = useState(0)
+        const [userData, setUserData] = useState({})
+        const userDoc = doc(firestore, 'users/' + auth.currentUser.uid)
 
-    useEffect(() => {
-        let intervalId = setInterval(() => setTime(prevTime => prevTime + 1), 1000)
-        return () => clearInterval(intervalId) // Clear interval on unmount
+        useEffect(() =>{
+            const getUserData = async () =>{
+                try{
+                    const data = (await getDoc(userDoc)).data()
+                    setUserData(data)
+                    console.log(data)
 
-    }, []);
+                }
+                catch(error){
+                    //nice error message, have fun figuring out what oops
+                    //means in your console when something breaks lol
+                    console.error('oops')
+                }
+            }
+            getUserData()
+        },[])
 
-    useEffect(() => {
-        const writeData = async () =>{
-            const now = new Date();
-            const today = now.toISOString().split('T')[0];
-            console.log(today);
+        useEffect(() => {
+            let intervalId = setInterval(() => setTime(prevTime => prevTime + 1), 1000)
+            return () => clearInterval(intervalId) // Clear interval on unmount
+
+        }, []);
+
+        useEffect(() => {
+            const writeData = async () =>{
+                const now = new Date();
+                const today = now.toISOString().split('T')[0];
+                console.log(today);
+                const userData = {
+                    lastSessionDate: today
+                }
+                try{
+                    setDoc(userDoc,userData, {merge: true})
+                } catch(error){
+                    console.error('HMMMMM')
+                }  
+            }
+            
+            writeData()
+        }, [])
+
+        const hours = String(Math.floor(time / 3600)).padStart(2, '0')
+        const minutes = String(Math.floor((time % 3600) / 60)).padStart(2, '0')
+        const seconds = String(time % 60).padStart(2, '0')
+
+        /*
+        useEffect(() =>{
             const userData = {
-                lastSessionDate: today
+                
             }
-            try{
-                setDoc(userDoc,userData, {merge: true})
-            } catch(error){
-                console.error('HMMMMM')
-            }
-            
-        }
-        
-        writeData()
-    }, [])
-
-    const hours = String(Math.floor(time / 3600)).padStart(2, '0')
-    const minutes = String(Math.floor((time % 3600) / 60)).padStart(2, '0')
-    const seconds = String(time % 60).padStart(2, '0')
-
-    /*
-    useEffect(() =>{
-        const userData = {
-            
-        }
-    },[minutes])
-    */
-    return(
-        <> 
-            <div className='flex flex-col items-center justify-center h-screen'>
-                <img src={auth.currentUser.photoURL} className="rounded-full m-4"></img>
-                <p>Hello, {auth.currentUser.displayName}</p>
-                <h1 className="text-9xl m-5">
-                {hours}:{minutes}:{seconds}
-                </h1>
-                <h4 className="">Current Session</h4>
-                <button onClick={() => auth.signOut()} className="border border-spacing-2 p-2 bg-or m-4">Sign Out</button>
-            </div>
-        </> 
-        
-        
-    )
-}
+        },[minutes])
+        */
+        return(
+            <> 
+                <div className='flex flex-col items-center justify-center h-screen'>
+                    <img src={auth.currentUser.photoURL} className="rounded-full m-4"></img>
+                    <p>Hello, {auth.currentUser.displayName}</p>
+                    <h1 className="text-9xl m-5">
+                    {hours}:{minutes}:{seconds}
+                    </h1>
+                    <h4 className="m-2">Current Session</h4>
+                    <p>Last Session: {userData.lastSessionDate}</p>
+                    <button onClick={() => auth.signOut()} className="border border-spacing-2 p-2 bg-or m-4">Sign Out</button>
+                </div>
+            </>  
+        )
+    }
--- Frontend/src/utils/firebase.js
+++ Frontend/src/utils/firebase.js
@@ -1,7 +1,7 @@
 //TODO: FIX THIS COMPAT SDK MESS FFS
 import firebase from 'firebase/compat/app'; 
 import 'firebase/compat/auth'; 
-import {getFirestore, doc, setDoc} from 'firebase/firestore'
+import {getFirestore, doc, setDoc, getDoc} from 'firebase/firestore'
 
 const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyCL9kCO15o9TySDbIvaD4tHrdEqC-c0FHw",
@@ -16,5 +16,5 @@ const firebaseApp = firebase.initializeApp({
 export const firestore = getFirestore()
 export const auth = firebaseApp.auth()
 export const firebaseAuth = firebase.auth
-export {doc, setDoc}
+export {doc, setDoc, getDoc}
 export default firebaseApp
